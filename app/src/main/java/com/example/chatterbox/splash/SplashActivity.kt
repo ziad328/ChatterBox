369ee8c31d96807f01778c2b6d6139895c5215ca
ziad328 <ziad32seleem@gmail.com>
2024-07-31 23:17:32 +0300

    + Splash Activity and Logo Design

--- app/src/main/java/com/example/chatterbox/splash/SplashActivity.kt
+++ app/src/main/java/com/example/chatterbox/splash/SplashActivity.kt
@@ -1,21 +1,39 @@
 package com.example.chatterbox.splash
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.chatterbox.R
+import com.example.chatterbox.databinding.ActivitySplashBinding
+import com.example.chatterbox.home.HomeActivity
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
+        viewBinding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        Handler(Looper.getMainLooper())
+            .postDelayed({
+                startHomeActivity()
+            }, 2000)
+
+    }
+
+    private fun startHomeActivity() {
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
